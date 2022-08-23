ebfaaeee50db5f0afd6372b6a692c7547df76e46
DevFaizaan <DevFaizaan.b@gmail.com>
2022-08-23 16:53:26 +0100

    states and stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState } from 'react';
 
-function App() {
+export default function App() {
+  const [counter, setCounter] = useState(0);
+  const [savedCounts, setSavedCounts] = useState([]);
+  //increase counter
+  const increaseOne = () => {
+    setCounter((count) => count + 1);
+  };
+
+  const increaseFive = () => {
+    setCounter((count) => count + 5);
+  };
+  //decrease counter
+  const decreaseOne = () => {
+    setCounter((count) => count - 1);
+  };
+
+  const decreaseFive = () => {
+    setCounter((count) => count - 5);
+  };
+
+  //reset counter
+  const reset = () => {
+    setCounter(0);
+  };
+
+  const saveCount = () => {
+    setSavedCounts([...savedCounts, counter]);
+    console.log(counter);
+    reset();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='counter'>
+      <h1>React Counter</h1>
+      <span className='counter__output'>{counter}</span>
+      <div className='btn__container'>
+        <button className='control__btn' onClick={decreaseFive}>
+          -5
+        </button>
+        <button className='control__btn' onClick={decreaseOne}>
+          -1
+        </button>
+        <button className='reset' onClick={reset}>
+          Reset
+        </button>
+        <button className='control__btn' onClick={increaseOne}>
+          +1
+        </button>
+        <button className='control__btn' onClick={increaseFive}>
+          +5
+        </button>
+        <button className='reset' onClick={saveCount}>
+          Save
+        </button>
+      </div>
+      <ul className='historyList'>
+        {savedCounts.map((saveCount, index) => (
+          <li key={index}>{saveCount}</li>
+        ))}
+      </ul>
     </div>
   );
 }
-
-export default App;
